4337f4c061c07f1edbb4795799902f7e93196c05
Thach Duong <Thach.Duong@halliburton.com>
2019-05-15 21:03:53 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild, Renderer, Inject, ElementRef } from '@angular/core';
+import * as AOS from 'aos';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'resume-angular';
+  @ViewChild (NavbarComponent) navbar: NavbarComponent;
+
+  constructor(
+    private renderer: Renderer, 
+    @Inject(DOCUMENT) private document: any, 
+    private element: ElementRef
+  ) { }
+
+  ngOnInit() {
+    // init AOS 
+    AOS.init();
+
+    // init now ui kit
+    let navbar: HTMLElement = this.element.nativeElement.children[0].children[0].children[0].children[0];
+    this.renderer.listenGlobal('window', 'scroll', (event) => {
+      const number = window.scrollY;
+      if (number > 500 || window.pageYOffset > 500) {
+        navbar.classList.remove('navbar-transparent');
+      } else {
+        navbar.classList.add('navbar-transparent');
+      }
+    });
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { AboutComponent } from './about/about.component';
+import { SkillComponent } from './skill/skill.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactComponent,
+    EducationComponent,
+    AboutComponent,
+    SkillComponent,
+    PortfolioComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
